997fb7c4ebcc9c4701989f83dad034792b327e6c
BhavyaG03 <gulatibhavya30@gmail.com>
2025-07-19 23:18:13 +0530

    pwa deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Website',
+        name: 'My Website App',
+        icons: [
+          {
+            src: 'logoM.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logoM.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4f46e5',
+      },
+    }),
+  ],
 })
